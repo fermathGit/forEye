70e6a6504a540dab33766dd01cf4b3b59a16a2d4
liuwenzhe <308166730@qq.com>
2017-11-04 16:02:48 +0800

    first add

---
+++ UnityProjects/New Unity Project In 2017/Assets/FurBall2D/Scripts/DestroyCloud.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCloud : MonoBehaviour {
+    public ObjectsPool pool;
+    // Use this for initialization
+    void Start(){
+
+		Invoke("DestroyIt",0.8f);
+	}
+
+	void DestroyIt(){
+        pool.ReturnInstance( gameObject);}
+    
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ UnityProjects/New Unity Project In 2017/Assets/FurBall2D/Scripts/ObjectsPool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+//https://zhuanlan.zhihu.com/p/30575559
+public class ObjectsPool : MonoBehaviour {
+    [SerializeField]
+    GameObject _prefab;
+
+    Queue<GameObject> _pooledInstanceQueue = new Queue<GameObject>();
+
+    public GameObject GetInstance() {
+        if ( _pooledInstanceQueue.Count > 0 ) {
+            GameObject instanceToReuse = _pooledInstanceQueue.Dequeue();
+            instanceToReuse.SetActive( true );
+            return instanceToReuse;
+        }
+        return Instantiate( _prefab );
+    }
+
+    public void ReturnInstance( GameObject gameObjectToPool ) {
+        _pooledInstanceQueue.Enqueue( gameObjectToPool );
+        gameObjectToPool.SetActive( false );
+        gameObjectToPool.transform.SetParent( transform );
+
+    }
+}
